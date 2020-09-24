9a2e5ed0f180cdabea7439f9dd4eed66f583dcc5
Crolder <artjoms98@gmail.com>
2020-09-24 09:45:09 +0300

    Basics homework

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+<h1>Evolution Scala Bootcamp Homework</h1>
+<ul>
+<li>Basics homework &#10003</li>
+</ul>
---
+++ src/main/scala/basics/gcd_lcm.scala
@@ -0,0 +1,17 @@
+package main.scala.basics
+
+object gcd_lcm extends App{
+    val gcdTestResult: Int = 9
+    val lcmTestResult: Int = 216
+
+    def gcd (a: Int, b: Int): Int = {
+        if(b == 0) a else gcd(b, a % b)
+    }
+
+    def lcm (a: Int, b: Int): Int = {
+        (a*b)/gcd(a,b)
+    }
+
+    println(s"Result of gcd function: ${gcd(81,18)}, should be: $gcdTestResult")
+    println(s"Result of gcd function: ${lcm(54,24)}, should be: $lcmTestResult")
+}
